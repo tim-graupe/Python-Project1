2955acd1df55bedfa94cfdb65e6a766ccb770a20
tim-graupe <77597601+tim-graupe@users.noreply.github.com>
2021-02-12 13:34:11 -0500

    Updated code
    Updated code

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,3 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
 
 import random
 
@@ -16,15 +6,15 @@ print("Welcome to the number guesser, {}. Your task is to guess the number betwe
 
 
 def start_game():
-    attempts = 0
+    attempts_left = 11
     ran_num = random.randint(1, 10)
-    guess = int(input("I am thinking of a number between 1 - 10, what is it?  "))
-    attempts +1
-    while guess != ran_num:
-        attempts +=1
+    while attempts_left != 0:
+        attempts_left -= 1
         try:
             guess = int(input("Guess the number, 1 - 10."))
-            if guess < 1:
+            if guess == ran_num:
+              break
+            elif guess < 1:
                 raise ValueError
             elif guess > 10:
                 raise ValueError
@@ -36,39 +26,22 @@ def start_game():
             print("Please enter a valid number.")
 
 
-    if attempts == 0:
+    if attempts_left == 10:
         print("You got it on the first try! Are you cheating?")
+    elif attempts_left == 0:
+        print("You are out of tries, better luck next time! The magic number was {}".format(ran_num))
     else:
-        print("Winner winner chicken dinner! It took you {} attempts to get it.".format(attempts + 1))
-    if attempts < high_score:
-        high_score == attempts
-        print("New high score!")
-    play_again = str(input("Would you like to play again? Y/N").upper())
+      print("Winner winner chicken dinner! It took you {} attempts to get it.".format(10 - attempts_left))
+    try:
+      
+      play_again = str(input("Would you like to play again? Y/N").upper())
+    except ValueError:
+           print("Please enter Y or N")
     if play_again == "Y":
-      print("Game on! Can you beat your previous high score of {}?".format(high_score))
-      start_game()
+        print("Game on!")
+        start_game()
     elif play_again == "N":
         print("That was fun! Hope we play again soon!")
-        
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
 
 
-
-# Kick off the program by calling the start_game function.
 start_game()
